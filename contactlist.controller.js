061f5741a0e52b70ff4d119d405100aa0f0593a7
Pesche007 <peter.windemann@gmx.ch>
2015-01-15 13:48:10 +0100

    Enabled contact groups

--- contactlist.controller.js
+++ contactlist.controller.js
@@ -1,8 +1,46 @@
 'use strict';
 
 angular.module('contacts')
-.controller('ContactsListCtrl', function ($scope, AppConfig) {
+.service('dataService', function($http) {
+	delete $http.defaults.headers.common['X-Requested-With'];
+	this.getData = function() {
+		return $http({
+			method: 'GET',
+			url: 'api/contacts',
+			params: '',
+			headers: {}
+		 });
+	 }
+})
+.controller('ContactsListCtrl', function ($scope, $http, AppConfig, dataService) {
 	AppConfig.setCurrentApp('Contacts', 'fa-user', 'contacts', 'app/contacts/menu.html');
+   
+    $scope.contacts = null;
+	$scope.contactsOPT={groupSel:null, contactSel:null, contactDisplay:null, contactEdit:false};
+	$scope.groupOPT={groups:[], groupcount:{}};
+    dataService.getData().then(function(dataResponse) {
+        $scope.contacts = dataResponse.data;
+		$scope.getGroups();
+    	});
+	$scope.getGroups=function(){
+		$scope.groups=[];
+		angular.forEach($scope.contacts, function(value) {
+			if(value.groups.length) {				
+				angular.forEach(value.groups, function(val) {
+					if(!$scope.groupOPT.groupcount[val.text]) {
+						$scope.groupOPT.groupcount[val.text]=0;
+						}
+					if($scope.groupOPT.groups.indexOf(val.text)===-1) {
+						$scope.groupOPT.groups.push(val.text);
+						}
+					$scope.groupOPT.groupcount[val.text]++;
+					});
+				}
+			});
+		}	
+	$scope.groupCheck=function(obj){
+		return obj.map(function(e) { return e.text}).indexOf($scope.contactsOPT.groupSel)!==-1;
+		}
+})
 
-});
 
