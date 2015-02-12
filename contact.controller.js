b0f72e0c98b562c46988bc54c5e10c7c4f0eb38c
Pesche007 <peter.windemann@gmx.ch>
2015-02-12 15:15:34 +0100

    Group Function V.10
    - Add
    - Delete
    - Animations with ngAnimate

--- contact.controller.js
+++ contact.controller.js
@@ -38,23 +38,34 @@ angular.module('contacts')
 		 });
 	};	
 })
-.controller('ContactsCtrl', function ($scope, $http, $filter, $log, cfg, dataService) {
+.controller('ContactsCtrl', function ($scope, $http, $filter, $log, $modal, cfg, dataService) {
 	cfg.GENERAL.CURRENT_APP = 'contacts';
 	//API   
 	$scope.API={};
 		
     $scope.contacts = null;	
-	$scope.contactsOPT={groupSel:null, contactDisplay:null, contactSave:null,  contactTmp:null, contactEdit:false, contactNew:0, contactNamesDisplay:null};
-	$scope.groupOPT={groupAdd:0, addGroupName:'', groups:[{label:'Business'},{label:'Private'}]};
-	$scope.sidemenu={m1:1, m2:1};					
-		
+	$scope.contactsOPT={contactDisplay:null, contactSave:null,  contactTmp:null, contactEdit:false, contactNew:0, contactNamesDisplay:null};
+	$scope.groupOPT={groupAdd:0, addGroupName:'', groupSel:null, groups:[{id:'abc', label:'Business'},{id:'def', label:'Private'}]};
+	$scope.globalEdit=false;
+
     dataService.getData().then(function(dataResponse) {
     	$log.log(dataResponse);
         //$scope.contacts = dataResponse.data;
 		//$scope.contactsOPT.contactNamesDisplay=$scope.API.getContactNames();				
 		//$scope.contactsShow($scope.contacts[0]);		
     	});
-
+    $scope.addGroup=function(){
+    	if($scope.groupOPT.groups.map(function(e){return e.label;}).indexOf($scope.groupOPT.addGroupName)===-1) {
+    		$scope.groupOPT.groups.push({label:$scope.groupOPT.addGroupName});
+    		}
+    	$scope.groupOPT.groupAdd=0;
+    	$scope.groupOPT.addGroupName='';
+    };
+    $scope.removeGroup=function(id){
+    	if($scope.groupOPT.groups.map(function(e){return e.id;}).indexOf(id)!==-1) {
+    		$scope.groupOPT.groups.splice($scope.groupOPT.groups.map(function(e){return e.id;}).indexOf(id), 1);
+    		}
+    }
 	$scope.addElementToGroup=function(e, i){
 		$scope.highlightOut(e);
 		var groupTarget=JSON.parse(e.target.dataset.options);
@@ -62,8 +73,12 @@ angular.module('contacts')
 		var groupSource=JSON.parse(i.draggable[0].dataset.options);
 		var contactID=groupSource.id;
 		console.log(groupID, contactID);
-
 	};
+	$scope.selGroup = function(id, $event){
+		if(!$scope.globalEdit){
+			$scope.groupOPT.groupSel=id;
+		}
+	}	
 	//Highlight D&D
 	$scope.highlightOver = function(e){
 		var eTarget=e.target;
